ee9400987b6f4b4e8d34fd2f4881542f784efa06
Joan Miralles <jmiralles@brujula.es>
2018-07-31 08:25:29 +0200

    Add some totals to report

--- readjson.js
+++ readjson.js
@@ -2,41 +2,42 @@ let fs = require("fs");
 let SortedMap = require("collections/sorted-map");
 let DateUtils = require("./lib/date_utils");
 console.log("\n *STARTING* \n");
+
 let dateMap = new SortedMap();
-
-function logValue(key) {
-    var value = dateMap.has(key) ? dateMap.get(key) : 0;
-    console.log(key + " => " + value);
-}
-
-var filename;
-var contents;
-// Get content from file
-for (var i = 0; i < 3700; i = i + 100) {
-    filename = "user-files/" + "users-" + i + ".json";
+for (let i = 0; i < 3700; i = i + 100) {
+    // Get content from file
+    let filename = "user-files/" + "users-" + i + ".json";
+    let contents;
     try {
         contents = fs.readFileSync(filename);
     } catch (e) {
         break;
     }
-    // Define to JSON type
-    var jsonContent = JSON.parse(contents);
+    let jsonContent = JSON.parse(contents);
 
-    for (var index in jsonContent) {
-        var item = jsonContent[index];
-        var registerDate = new Date(item.createdTimestamp);
-        var registerDateOnlyDay = DateUtils.getFormattedDate(registerDate);
-        if (dateMap.has(registerDateOnlyDay)) {
-            dateMap.set(registerDateOnlyDay, dateMap.get(registerDateOnlyDay) + 1);
-        } else {
-            dateMap.set(registerDateOnlyDay, 1);
+    for (let index in jsonContent) {
+        let item = jsonContent[index];
+        let registerDate = new Date(item.createdTimestamp);
+        let registerDateOnlyDay = DateUtils.getFormattedDate(registerDate);
+        if (!dateMap.has(registerDateOnlyDay)) {
+            dateMap.set(registerDateOnlyDay, 0);
         }
+        dateMap.set(registerDateOnlyDay, dateMap.get(registerDateOnlyDay) + 1);
     }
 }
-console.log("\n *TOTAL:* " + dateMap.values().sum());
-var sortedKeys = dateMap.keys().sorted();
-var firstDate = new Date(sortedKeys[0]);
-var lastDate = new Date(sortedKeys[sortedKeys.length - 1]);
-var allDays = DateUtils.getDateArray(firstDate, lastDate).map(DateUtils.getFormattedDate);
-allDays.forEach(logValue);
+let sortedKeys = dateMap.keys().sorted();
+let firstDate = new Date(sortedKeys[0]);
+let lastDate = new Date(sortedKeys[sortedKeys.length - 1]);
+console.log("\n Data primer usuari: " + firstDate);
+console.log("\n Data darrer usuari: " + lastDate);
+console.log("\n 17 Juliol: " + dateMap.get('2018-07-17'));
+console.log("\n 18 Juliol: " + dateMap.get('2018-07-18'));
+
+let allDaysIn2018 = DateUtils.getDateArray(new Date('2018-01-01'), new Date('2018-07-30')).map(DateUtils.getFormattedDate);
+console.log("\n *TOTAL 2018:* " + allDaysIn2018.map(function (key) {
+    return dateMap.has(key) ? dateMap.get(key) : 0;
+}).sum());
+console.log("\n *TOTAL DAYS 2018:* " + allDaysIn2018.length);
+
+console.log("\n *TOTAL EXTRANET:* " + dateMap.values().sum());
 console.log("\n *EXIT* \n");
